3595a15cd78957bd06ad17dbb665a9ee30697f87
paularg14 <80535456+paularg14@users.noreply.github.com>
2021-12-20 01:14:23 +0100

    Carga de archivos + instalación de paquetes

--- Chemmine.R
+++ Chemmine.R
@@ -1,1 +1,17 @@
+# Importación de Datos ----------------------------------------------------
 
+
+# Se importan las librerías que se van a utilizar
+library(tidyverse)
+# Librerías ya incluidas en tidyverse:
+#library(readr)
+#library(dplyr)
+
+# Carga de datos de ejemplo de ChemmineR
+Datos_ChemmineR <- read_csv("INPUT/DATA/sdfsample.sdf")
+Datos_ChemmineR #Vemos que los datos son correctos
+
+#Se instala el paquete BiocManager, dentro del cual se encuentra ChemmineR
+install.packages("BiocManager")
+library(BiocManager)
+BiocManager::install("ChemmineR")
